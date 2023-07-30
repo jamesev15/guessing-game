ec2fd8458c85939f29eb217f1f4d4f61db62fa50
James Espichan Vilca <84110446+jamesev15@users.noreply.github.com>
2023-07-30 18:59:05 +0000

    Adding gpt clue

--- src/main.rs
+++ src/main.rs
@@ -108,17 +108,15 @@ fn generate_secret_number_gpt(start: u32, end: u32) -> u32 {
     secret_number
 }
 
-fn handler_secret_number() -> fn(u32, u32) -> u32 {
-    let openai_token = env::var("OPENAI_TOKEN").unwrap_or_default();
-
-    if openai_token == "".to_string() {
-        println!("OPENAI TOKEN not found!");
-        println!("Generating secret number with local CPU");
-        generate_secret_number
-    } else {
+fn handler_secret_number(gpt: bool) -> fn(u32, u32) -> u32 {
+    if gpt {
         println!("OPENAI TOKEN found!");
         println!("Generating secret number with GPT3.5");
         generate_secret_number_gpt
+    } else {
+        println!("OPENAI TOKEN not found!");
+        println!("Generating secret number with local CPU");
+        generate_secret_number
     }
 }
 
@@ -130,8 +128,27 @@ impl Game {
     fn new() -> Self {
         println!("Welcome to the guessing game powered by GPT");
 
-        let fn_handler = handler_secret_number();
+        let mut gpt: bool = false;
+        let openai_token = env::var("OPENAI_TOKEN").unwrap_or_default();
+
+        if openai_token != "".to_string() {
+            gpt = true;
+        }
+
+        let fn_handler = handler_secret_number(gpt);
         let secret_number = fn_handler(0, 50);
+
+        if gpt {
+            match gpt_completion(format!("Tell me a funny clue that helps me to guess the secret number. Just returns the clue without the secret number nor the clue's explanation.: {}", secret_number)){
+                Ok(clue) => {
+                    println!("GPT Clue: {}", clue);
+                },
+                Err(e) => {
+                    println!("Error generating clue: {}", e);
+                } 
+            };
+            
+        }
 
         Self { secret_number }
     }
