9d0776c4acac7850e67e96cb5ed98e0b7e9b9fb7
James Espichan Vilca <84110446+jamesev15@users.noreply.github.com>
2023-07-30 18:47:53 +0000

    Strategy pattern functional for the secret number generation

--- src/main.rs
+++ src/main.rs
@@ -1,123 +1,149 @@
-// use rand::Rng;
+use rand::Rng;
+use reqwest;
 use serde::Deserialize;
 use serde::Serialize;
-use std::cmp::Ordering;
-use std::io;
-use std::env;
 use serde_json;
-use reqwest::blocking;
+use std::cmp::Ordering;
+use std::env;
+use std::io;
 
-#[derive(Debug)]
-#[derive(Serialize, Deserialize)]
-struct GptMessage{
+#[derive(Debug, Serialize, Deserialize)]
+struct GptMessage {
     role: String,
     content: String,
 }
 #[derive(Serialize)]
-struct GptPayload{
+struct GptPayload {
     model: String,
     temperature: f32,
-    messages: Vec<GptMessage>
+    messages: Vec<GptMessage>,
 }
 
-#[derive(Debug)]
-#[derive(Deserialize)]
-struct GptChoice{
+#[derive(Debug, Deserialize)]
+struct GptChoice {
     message: GptMessage,
 }
 
-#[derive(Debug)]
-#[derive(Deserialize)]
-struct GptResponse{
-    choices: Vec<GptChoice>
+#[derive(Debug, Deserialize)]
+struct GptResponse {
+    choices: Vec<GptChoice>,
 }
 
-// fn generate_secret_number(start: u32, end: u32) -> u32{
-//     rand::thread_rng().gen_range(start..=end)
-// }
-
-fn generate_secret_number_gpt(start: u32, end: u32) -> u32{
-
-    let gpt_payload = GptPayload{
-        model: "gpt-3.5-turbo".to_string(),
-        temperature: 0.7,
-        messages: vec![GptMessage{
-            role: "user".to_string(),
-            content: format!("Return just a random number between {} and {} without explanations.", start, end),
-        }]
+fn gpt_completion(prompt: String) -> Result<String, String> {
+    let openai_token = match env::var("OPENAI_TOKEN") {
+        Ok(var) => var,
+        Err(_) => return Err("OPENAI Token not found.".to_string()),
     };
 
-    let gpt_payload = serde_json::to_string(&gpt_payload).expect("Error converting to JSON");
+    let gpt_payload = GptPayload {
+        model: "gpt-3.5-turbo".to_string(),
+        temperature: 0.7,
+        messages: vec![GptMessage {
+            role: "user".to_string(),
+            content: prompt,
+        }],
+    };
 
-    // Reading OPENAI_TOKEN env var
-    let openai_token = env::var("OPENAI_TOKEN").unwrap_or_default();
+    let gpt_payload = match serde_json::to_string(&gpt_payload) {
+        Ok(s) => s,
+        Err(_) => return Err("error generating gpt payload".to_string()),
+    };
 
     let client = reqwest::blocking::Client::new();
 
-    let response = client
-            .post("https://api.openai.com/v1/chat/completions".to_string())
-            .header("Content-Type", "application/json")
-            .header("Authorization", format!("Bearer {}", openai_token))
-            .body(gpt_payload)
-            .send()
-            .expect("Error calling GPT");
+    let response = match client
+        .post("https://api.openai.com/v1/chat/completions".to_string())
+        .header("Content-Type", "application/json")
+        .header("Authorization", format!("Bearer {}", openai_token))
+        .body(gpt_payload)
+        .send()
+    {
+        Ok(r) => r,
+        Err(_) => return Err("Error calling OPENAI URL".to_string()),
+    };
 
-    if response.status().is_success(){
-        let response_text = response.text().unwrap_or_default();
-        let gpt_response: GptResponse = serde_json::from_str(&response_text).expect("Error parsing GPT response");
-        let secret_number: u32 = gpt_response.choices[0].message.content.parse().unwrap_or(50);
+    if !response.status().is_success() {
+        return Err(format!("Error status code: {}", response.status()));
+    } else {
+        let response_text = match response.text() {
+            Ok(r) => r,
+            Err(_) => return Err("Error reading GPT response".to_string()),
+        };
 
-        return secret_number;
-    }
+        let gpt_response: GptResponse = match serde_json::from_str(&response_text) {
+            Ok(g) => g,
+            Err(_) => return Err("Error converting to json".to_string()),
+        };
 
-    50
-
-
+        return Ok(gpt_response.choices[0].message.content.clone());
+    };
 }
 
-fn read_debug_env_var(env_var: &str) -> bool{
-    env::var(env_var)
-            .unwrap_or("false".to_string())
-            .parse::<bool>()
-            .unwrap_or_default()
+fn generate_secret_number(start: u32, end: u32) -> u32 {
+    rand::thread_rng().gen_range(start..=end)
+}
+
+fn generate_secret_number_gpt(start: u32, end: u32) -> u32 {
+    let secret_number_default: u32 = 50;
+
+    let gpt_response = match gpt_completion(format!(
+        "Return just a random number between {} and {} without explanations.",
+        start, end
+    )) {
+        Ok(r) => r,
+        Err(e) => {
+            println!("Error calling gpt {}", e);
+            return secret_number_default;
+        }
+    };
+
+    let secret_number: u32 = match gpt_response.parse() {
+        Ok(n) => n,
+        Err(_) => {
+            println!("Error parsing number from gpt");
+            return secret_number_default;
+        }
+    };
+
+    secret_number
+}
+
+fn handler_secret_number() -> fn(u32, u32) -> u32 {
+    let openai_token = env::var("OPENAI_TOKEN").unwrap_or_default();
+
+    if openai_token == "".to_string() {
+        println!("OPENAI TOKEN not found!");
+        println!("Generating secret number with local CPU");
+        generate_secret_number
+    } else {
+        println!("OPENAI TOKEN found!");
+        println!("Generating secret number with GPT3.5");
+        generate_secret_number_gpt
+    }
 }
 
 struct Game {
-    debug: bool,
     secret_number: u32,
 }
 
-impl Game{
+impl Game {
+    fn new() -> Self {
+        println!("Welcome to the guessing game powered by GPT");
 
-    const GAME_DEBUG_NAME: &str = "GAME_DEBUG";
+        let fn_handler = handler_secret_number();
+        let secret_number = fn_handler(0, 50);
 
-    fn new() -> Self{
-        // println! is a macro
-        println!("Initializing game");
-        let secret_number = generate_secret_number_gpt(1, 100);
-
-        Self { 
-            debug: read_debug_env_var(Game::GAME_DEBUG_NAME),
-            secret_number,
-        }
+        Self { secret_number }
     }
 
     fn start(&self) {
-
-        if self.debug {
-            println!("[DEBUG] Secret number: {}", self.secret_number);
-        }
-
         loop {
             println!("-- Input your guess -- :");
-            // variable to mutate
+
             let mut guess = String::new();
-            
-            // read_line() returns a Result enum: Ok, Err
+
             io::stdin().read_line(&mut guess).expect("Error");
-    
-            // overshadow guess String variable
-            // parse() returns a Result enum: Ok, Err
+
             let guess: u32 = match guess.trim().parse() {
                 Ok(num) => num,
                 Err(_) => {
@@ -125,8 +151,7 @@ impl Game{
                     continue;
                 }
             };
-    
-            // Compare two numbers using pattern matching
+
             match guess.cmp(&self.secret_number) {
                 Ordering::Less => println!("To small"),
                 Ordering::Greater => println!("To big"),
@@ -140,8 +165,6 @@ impl Game{
 }
 
 fn main() {
-    
     let game = Game::new();
     game.start();
-
 }
